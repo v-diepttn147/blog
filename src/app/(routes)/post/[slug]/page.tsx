0f68d1ffea9b14cbe5ea329b79e978bfd2ff12cd
nhanluongoe <nhanlt18@gmail.com>
2023-07-23 11:51:15 +0700

    chore: comment out unused code

--- src/app/(routes)/post/[slug]/page.tsx
+++ src/app/(routes)/post/[slug]/page.tsx
@@ -1,35 +1,35 @@
-import PostBody from "@/common/components/PostBody";
-import markdownToHtml from "@/common/helper/markdownToHtml";
-import { getAllPosts, getPostBySlug } from "@/lib/posts";
-
-async function getPost(slug: string) {
-  const post = getPostBySlug(slug, [
-    "title",
-    "date",
-    "slug",
-    "author",
-    "content",
-    "ogImage",
-    "coverImage",
-  ]);
-  const content = await markdownToHtml(post.content || "");
-  return {...post, content};
-}
-
-export async function generateStaticParams() {
-  const posts = getAllPosts(["slug"]);
- 
-  return posts.map((post) => ({
-    slug: post.slug,
-  }));
-}
-
-export default async function PostPage({
-  params: { slug },
-}: {
-  params: { slug: string };
-}) {
-  const post = await getPost(slug);
-
-  return <div><PostBody content={post.content}/></div>;
-}
+// import PostBody from "@/common/components/PostBody";
+// import markdownToHtml from "@/common/helper/markdownToHtml";
+// import { getAllPosts, getPostBySlug } from "@/lib/posts";
+//
+// async function getPost(slug: string) {
+//   const post = getPostBySlug(slug, [
+//     "title",
+//     "date",
+//     "slug",
+//     "author",
+//     "content",
+//     "ogImage",
+//     "coverImage",
+//   ]);
+//   const content = await markdownToHtml(post.content || "");
+//   return {...post, content};
+// }
+//
+// export async function generateStaticParams() {
+//   const posts = getAllPosts(["slug"]);
+//  
+//   return posts.map((post) => ({
+//     slug: post.slug,
+//   }));
+// }
+//
+// export default async function PostPage({
+//   params: { slug },
+// }: {
+//   params: { slug: string };
+// }) {
+//   const post = await getPost(slug);
+//
+//   return <div><PostBody content={post.content}/></div>;
+// }
--- src/app/(routes)/post/page.tsx
+++ src/app/(routes)/post/page.tsx
@@ -1,12 +1,12 @@
-import { getAllPosts } from "@/app/_lib/posts";
-
-async function getPost() {
-  return getAllPosts();
-}
-
-export default async function Posts() {
-  const data = await getPost();
-
-  console.log(data, 'hihi')
-  return <div>Posts</div>;
-}
+// import { getAllPosts } from "@/app/_lib/posts";
+//
+// async function getPost() {
+//   return getAllPosts();
+// }
+//
+// export default async function Posts() {
+//   const data = await getPost();
+//
+//   console.log(data, 'hihi')
+//   return <div>Posts</div>;
+// }
