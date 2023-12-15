c28071b0a40f74ac54f0ea51cad91ea9baf2e50c
nhanluongoe <nhanlt18@gmail.com>
2023-12-15 21:21:25 +0700

    refactor: group related code

--- src/app/_common/hooks/useMousePosition.ts
+++ src/app/_common/hooks/useMousePosition.ts
@@ -5,11 +5,10 @@ import { useEffect, useState } from "react";
 export default function useMousePosition() {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
-  const updateMousePosition = (e: MouseEvent) => {
-    setMousePosition({ x: e.clientX, y: e.clientY });
-  };
-
   useEffect(() => {
+    const updateMousePosition = (e: MouseEvent) => {
+      setMousePosition({ x: e.clientX, y: e.clientY });
+    };
     window.addEventListener("mousemove", updateMousePosition);
     return () => window.removeEventListener("mousemove", updateMousePosition);
   }, []);
