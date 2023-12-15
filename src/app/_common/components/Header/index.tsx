ed9c5f56b7193bb4b3a2cdf550b0b1cbf496b86e
nhanluongoe <nhanlt18@gmail.com>
2023-12-15 21:48:55 +0700

    feat(homepage): add selected indicator for navigation

--- src/app/_common/components/Header/index.tsx
+++ src/app/_common/components/Header/index.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export interface INavItem {
   id: string | number;
@@ -25,6 +28,8 @@ const NAV_ITEMS: INavItem[] = [
 ];
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
     <header className="flex justify-between items-start pb-8">
       <Link href="/" className="text-2xl font-semibold pb-2 text-zinc-700">
@@ -33,7 +38,14 @@ export default function Header() {
       <ul className="flex gap-x-2 text-zinc-500">
         {NAV_ITEMS.map((item) => (
           <li key={item.id}>
-            <Link href={item.value} className="">
+            <Link
+              href={item.value}
+              className={`opacity-75 ${
+                pathname.includes(item.value)
+                  ? "opacity-100 border-b-2 border-b-zinc-400"
+                  : ""
+              } hover:opacity-100 hover:border-b-2 hover:border-b-zinc-400 pb-1`}
+            >
               {item.label}
             </Link>
           </li>
