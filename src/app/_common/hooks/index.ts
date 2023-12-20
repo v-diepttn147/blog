0b0ca880408989f6375a4ba53f04b7097ad8948f
nhanluongoe <nhanlt18@gmail.com>
2023-12-20 10:43:29 +0700

    Use a template

--- src/app/_common/hooks/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as useMousePosition } from "./useMousePosition";
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,71 @@
-/** @type {import('tailwindcss').Config} */
+// @ts-check
+const { fontFamily } = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
+/** @type {import("tailwindcss/types").Config } */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './node_modules/pliny/**/*.js',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,tsx}',
+    './components/**/*.{js,ts,tsx}',
+    './layouts/**/*.{js,ts,tsx}',
+    './data/**/*.mdx',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      lineHeight: {
+        11: '2.75rem',
+        12: '3rem',
+        13: '3.25rem',
+        14: '3.5rem',
       },
-      animation: {
-        flash: "flash 3s linear infinite",
+      fontFamily: {
+        sans: ['var(--font-space-grotesk)', ...fontFamily.sans],
       },
-      keyframes: {
-        flash: {
-          "0%, 100%": { opacity: 1 },
-          "50%": { opacity: 0 },
+      colors: {
+        primary: colors.pink,
+        gray: colors.gray,
+      },
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            a: {
+              color: theme('colors.primary.500'),
+              '&:hover': {
+                color: `${theme('colors.primary.600')}`,
+              },
+              code: { color: theme('colors.primary.400') },
+            },
+            'h1,h2': {
+              fontWeight: '700',
+              letterSpacing: theme('letterSpacing.tight'),
+            },
+            h3: {
+              fontWeight: '600',
+            },
+            code: {
+              color: theme('colors.indigo.500'),
+            },
+          },
         },
-      },
-    },
-    container: {
-      screens: {
-        sm: "640px",
-        md: "768px",
-        lg: "1280px",
-      },
+        invert: {
+          css: {
+            a: {
+              color: theme('colors.primary.500'),
+              '&:hover': {
+                color: `${theme('colors.primary.400')}`,
+              },
+              code: { color: theme('colors.primary.400') },
+            },
+            'h1,h2,h3,h4,h5,h6': {
+              color: theme('colors.gray.100'),
+            },
+          },
+        },
+      }),
     },
   },
-  plugins: [],
-};
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+}
