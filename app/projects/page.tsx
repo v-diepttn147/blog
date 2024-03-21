d5c61bf02b1420274e01e0fc1147a3816a78a06b
nhanluongoe <nhanlt18@gmail.com>
2024-03-21 22:40:57 +0700

    project: add react-stacked-toast

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,5 @@
+import Card from '@/components/card'
+import projectsData from '@/data/projectsData'
 import { genPageMetadata } from 'app/seo'
 
 export const metadata = genPageMetadata({ title: 'Projects' })
@@ -7,16 +9,13 @@ export default function Projects() {
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="md:leading-14 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl">
+          <h1 className="md:leading-14 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:leading-10">
             Projects
           </h1>
-          <p className="text-lg leading-7 text-gray-900 dark:text-gray-100">
-            These are some of the projects I've worked on.
-          </p>
         </div>
         <div className="container py-12">
           <div className="-m-4 flex flex-wrap">
-            {/* {projectsData.map((d) => (
+            {projectsData.map((d) => (
               <Card
                 key={d.title}
                 title={d.title}
@@ -24,7 +23,7 @@ export default function Projects() {
                 imgSrc={d.imgSrc}
                 href={d.href}
               />
-            ))} */}
+            ))}
           </div>
         </div>
       </div>
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,11 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'react-stacked-toast',
+    description: `A simple and flexible stacked toast library for React. It's easy to use and
+    customizable. 
+    `,
+    imgSrc: '/static/images/react-stacked-toast.png',
+    href: 'https://react-stacked-toast.vercel.app',
   },
 ]
 
