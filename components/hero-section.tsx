cb2f29e5b654f63537debb574a7a0f1bd4e1791b
nhanluongoe <nhanlt18@gmail.com>
2024-03-21 22:40:37 +0700

    chore: update ui

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 export default function HeroSection() {
   return (
     <section className="my-10 flex flex-col items-center justify-center text-center">
-      <h1 className="bg-gradient-to-r from-[#E84B3C] to-[#5cb5f0] bg-clip-text text-8xl font-bold leading-normal text-transparent">
+      <h1 className="bg-gradient-to-r from-[#AB271E] to-[#2B5F8C] bg-clip-text text-7xl font-bold leading-normal text-transparent dark:from-[#E84B3C] dark:to-[#5cb5f0]">
         Blogs.
       </h1>
-      <h1 className="bg-gradient-to-r from-[#E84B3C] to-[#5cb5f0] bg-clip-text text-8xl font-bold leading-normal text-transparent">
+      <h1 className="bg-gradient-to-r from-[#AB271E] to-[#2B5F8C] bg-clip-text text-7xl font-bold leading-normal text-transparent dark:from-[#E84B3C] dark:to-[#5cb5f0]">
         Open Source.
       </h1>
     </section>
