7ae7603beef70605171a8fe63ac4ec6a0cf2473d
nhanluongoe <nhanlt18@gmail.com>
2024-03-21 09:55:01 +0700

    fix: make text center

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function HeroSection() {
   return (
-    <section className="my-10 flex flex-col items-center justify-center">
+    <section className="my-10 flex flex-col items-center justify-center text-center">
       <h1 className="bg-gradient-to-r from-[#E84B3C] to-[#5cb5f0] bg-clip-text text-8xl font-bold leading-normal text-transparent">
         Blogs.
       </h1>
