3a92dcdab685155d798422f3c56cbc5348fb2040
nhanluongoe <nhanlt18@gmail.com>
2024-04-21 13:55:36 +0700

    fix: correct image path

--- components/image.tsx
+++ components/image.tsx
@@ -1,5 +1,11 @@
+// import NextImage, { ImageProps } from 'next/image'
+
+// const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
+
+// export default Image
+
 import NextImage, { ImageProps } from 'next/image'
 
-const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
+const Image = ({ src, ...rest }: ImageProps) => <NextImage src={`/blog${src}`} {...rest} />
 
 export default Image
