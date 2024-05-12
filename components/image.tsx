f8bd7e1ed8bb3640c740f5c0fd2a3e5a087c06b0
nhanluongoe <nhanlt18@gmail.com>
2024-05-12 16:05:31 +0700

    Revert "fix: correct image path"
    This reverts commit 3a92dcdab685155d798422f3c56cbc5348fb2040.

--- components/image.tsx
+++ components/image.tsx
@@ -1,11 +1,5 @@
-// import NextImage, { ImageProps } from 'next/image'
-
-// const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
-
-// export default Image
-
 import NextImage, { ImageProps } from 'next/image'
 
-const Image = ({ src, ...rest }: ImageProps) => <NextImage src={`/blog${src}`} {...rest} />
+const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
 
 export default Image
