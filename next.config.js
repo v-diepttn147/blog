ef2a55fab4bffe3426d8d3c4b63477fb3469205e
Diep Tran <gau147@Dieps-MacBook-Pro.local>
2024-05-12 15:43:41 +0700

    build: update base bath

--- next.config.js
+++ next.config.js
@@ -60,7 +60,6 @@ const securityHeaders = [
 module.exports = () => {
   const plugins = [withContentlayer, withBundleAnalyzer]
   return plugins.reduce((acc, next) => next(acc), {
-    basePath: '/blog',
     output: 'export',
     reactStrictMode: true,
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
