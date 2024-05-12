34c963c077db86dad980c2bc23ecabd5ff4a9167
diepttn147 <diepttn147@gmail.com>
2024-05-12 22:59:08 +0700

    add lowlight project

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,12 +1,19 @@
 const projectsData = [
+  // {
+  //   title: 'react-stacked-toast',
+  //   repo: 'react-stacked-toast',
+  //   description: `A simple and flexible stacked toast library for React. It's easy to use and
+  //   customizable. 
+  //   `,
+  //   href: 'https://react-stacked-toast.vercel.app',
+  //   year: 2023,
+  // },
   {
-    title: 'react-stacked-toast',
-    repo: 'react-stacked-toast',
-    description: `A simple and flexible stacked toast library for React. It's easy to use and
-    customizable. 
-    `,
-    href: 'https://react-stacked-toast.vercel.app',
-    year: 2023,
+    title: 'Lowlight Image Enhancement',
+    repo: 'hdrcam',
+    description: `Lowlight Image Enhancement method built in Android application`,
+    href: 'https://github.com/v-diepttn147/hdrcam',
+    year: 2021,
   },
 ]
 
